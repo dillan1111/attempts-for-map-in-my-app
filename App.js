1e295ae076929953830e509fb03d16c6e9018c15
dillan1111 <70030386+dillan1111@users.noreply.github.com>
2021-07-17 14:07:44 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text} from 'react-native';
+import MapContainer from './components/GoogleMap'
+
+export default function App() {
+  return (
+    <View >
+      <Text style={{fontSize: 30, fontWeight: 'bold',color: 'black',}}>Google Maps App</Text>
+      <MapContainer/>
+    </View>
+  );
+    
+}
